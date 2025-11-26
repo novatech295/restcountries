f8003a4e8ccc30b1bf02809391c7ffa26975fbbd
novatech295 <novatech295@gmail.com>
2025-11-26 17:05:03 +0300

    Second Commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import NavBar from './Navbar/Navbar';
 
 import { Routes, Route } from 'react-router-dom';
 import Details from './Details/Details';
+
 export const CounterContext = createContext();
+
 function App() {
   const [count, setCount] = useState("Filter by Region");
   const [search, setSearch] = useState("");
   const [isDarkMode, setIsDarkMode] = useState(false);
+
   return (
- 
-      <CounterContext.Provider value={{ count, setCount, search, setSearch, isDarkMode, setIsDarkMode }}>
-        <NavBar />
-        <Routes>
-          <Route path='rest-countries' element={<Main />}></Route>
-          <Route path='rest-countries/details/:alpha3Code' element={<Details />}></Route>
-        </Routes>
-      </CounterContext.Provider>
+    <CounterContext.Provider
+      value={{ count, setCount, search, setSearch, isDarkMode, setIsDarkMode }}
+    >
+      <NavBar />
+
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/details/:alpha3Code' element={<Details />} />
+      </Routes>
+    </CounterContext.Provider>
   );
 }
 
